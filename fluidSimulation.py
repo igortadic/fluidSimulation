c40c324bd03cc0f7b9b8a70f89bd33f81c03bb60
TehStud <igortadic.msc@gmail.com>
2022-09-13 14:04:47 +0200

    spawning particles at randomizes trajectories

--- fluidSimulation.py
+++ fluidSimulation.py
@@ -23,11 +23,39 @@ FIGURE_SIZE = (4, 6)
 PLOT_EVERY = 6
 SCATTER_DOT_SIZE = 2000
 
+DOMAIN_X_LIM = np.array([
+	SMOOTHING_LENGHT,
+	DOMAIN_WIDTH - SMOOTHING_LENGHT,
+])
+DOMAIN_Y_LIM = np.array([
+	SMOOTHING_LENGHT,
+	DOMAIN_HEIGHT - SMOOTHING_LENGHT,
+])
+
+
 def main():
 	n_particles = 1
 
 
 	positions = np.zeros((n_particles, 2))
+	velocities = np.zeros_like(positions)
+	forces = np.zeros_like(positions)
+
+	for iter in tqdm(range(N_TIME_STEPS)):
+		if iter & ADD_PARTICLES_EVERY == 0 and n_particles < MAX_PARTICLES:
+			new_positions = np.array([
+				[10 + np.random.rand(), DOMAIN_Y_LIM[1]],
+				[15 + np.random.rand(), DOMAIN_Y_LIM[1]],
+				[20 + np.random.rand(), DOMAIN_Y_LIM[1]],
+			])
+
+			new_velocities = np.array([
+				[-3.0, -15.0],
+				[-3.0, -15.0],
+				[-3.0, -15.0],
+			])
+
+			n_particles += 3
 
 if __name__ = "__main__":
 	main()
